100b39f2f75416702ffef2aea48892828a75cff4
Aaryan Pashine <pashineaaryan@gmail.com>
2026-07-05 02:57:05 -0400

    added opencode models for dropdown

---
+++ models.js
@@ -0,0 +1,86 @@
+export const models = [
+  {
+    "name": "GLM-5.2",
+    "id": "glm-5.2",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "GLM-5.1",
+    "id": "glm-5.1",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "Kimi K2.7 Code",
+    "id": "kimi-k2.7-code",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "Kimi K2.6",
+    "id": "kimi-k2.6",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "DeepSeek V4 Pro",
+    "id": "deepseek-v4-pro",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "DeepSeek V4 Flash",
+    "id": "deepseek-v4-flash",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "MiMo-V2.5",
+    "id": "mimo-v2.5",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "MiMo-V2.5-Pro",
+    "id": "mimo-v2.5-pro",
+    "api": "opencode",
+    "sdk": "@ai-sdk/openai-compatible"
+  },
+  {
+    "name": "MiniMax M3",
+    "id": "minimax-m3",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  },
+  {
+    "name": "MiniMax M2.7",
+    "id": "minimax-m2.7",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  },
+  {
+    "name": "MiniMax M2.5",
+    "id": "minimax-m2.5",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  },
+  {
+    "name": "Qwen3.7 Max",
+    "id": "qwen3.7-max",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  },
+  {
+    "name": "Qwen3.7 Plus",
+    "id": "qwen3.7-plus",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  },
+  {
+    "name": "Qwen3.6 Plus",
+    "id": "qwen3.6-plus",
+    "api": "anthropic",
+    "sdk": "@ai-sdk/anthropic"
+  }
+];
